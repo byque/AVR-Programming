d45fb460b8c812669504e645ace8eba9abf18cc2
manosaloscables <manosaloscables@gmail.com>
2016-01-22 01:00:51 -0500

    Archivo make funciona, usar: make install

--- Chapter02_Programming-AVRs/blinkLED/blinkLED.c
+++ Chapter02_Programming-AVRs/blinkLED/blinkLED.c
@@ -8,18 +8,18 @@
 int main(void) {
 
   // -------- Inits --------- //
-  DDRB |= 0b0000001;            /* Data Direction Register B:
+  DDRB |= 0b0001000;            /* Data Direction Register B:
                                    writing a one to the bit
                                    enables output. */
 
   // ------ Event loop ------ //
   while (1) {
 
-    PORTB = 0b00000001;          /* Turn on first LED bit/pin in PORTB */
-    _delay_ms(1000);                                           /* wait */
+    PORTB = 0b00001000;          /* Turn on first LED bit/pin in PORTB */
+    _delay_ms(500);                                           /* wait */
 
     PORTB = 0b00000000;          /* Turn off all B pins, including LED */
-    _delay_ms(1000);                                           /* wait */
+    _delay_ms(500);                                           /* wait */
 
   }                                                  /* End event loop */
   return (0);                            /* This line is never reached */
